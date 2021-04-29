b34ccb3ec6265142d98a767fae6dd0ebd4327081
Sameeksha Sharma <sameeksha.sharma@centrica.com>
2021-04-29 17:40:19 +0530

    helper functions, component state and actions implementation

---
+++ app/components/counter.js
@@ -0,0 +1,16 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class CounterComponent extends Component {
+  @tracked count = 0;
+
+  get total() {
+    return this.count * this.args.multiple;
+  }
+
+  @action
+  change(amount) {
+    this.count = this.count + amount;
+  }
+}
